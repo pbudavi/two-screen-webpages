68a6ce36ac46134d1e791b5b5433de74b5c95c5d
Prajwal <prajwal.budavi@cynnent.com>
2024-01-30 21:34:39 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Screen1Component } from './screen1/screen1.component';
+import { Screen2Component } from './screen2/screen2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Screen1Component,
+    Screen2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
